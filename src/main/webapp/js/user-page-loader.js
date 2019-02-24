2a12057215ca13e6a2c1c96a664d5ec299ea5b63
GregVargas1999 <gregvargas1999@gmail.com>
2019-02-23 20:43:37 -0700

    Test commit with new servlet

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -27,6 +27,7 @@ if (!parameterUsername) {
 function setPageTitle() {
   document.getElementById('page-title').innerText = parameterUsername;
   document.title = parameterUsername + ' - User Page';
+  console.log("IS THIS SHOWING UP OR NAH");
 }
 
 /**
