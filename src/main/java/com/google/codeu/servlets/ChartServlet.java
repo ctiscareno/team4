80b21ae2d9292b8810b277caaea725c247897431
ctiscareno <ctisca1998@gmail.com>
2019-04-17 22:55:00 -0600

    'servlet'

--- src/main/java/com/google/codeu/servlets/ChartServlet.java
+++ src/main/java/com/google/codeu/servlets/ChartServlet.java
@@ -9,18 +9,27 @@ import java.util.List;
 import com.google.codeu.data.Message;
 import com.google.codeu.data.Migrant;
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 
 /**
  * Handles fetching site statistics.
  */
-@WebServlet("/messageChart")
+@WebServlet("/allChart")
 public class ChartServlet extends HttpServlet{
-
+	
+	JsonArray migrantArray;
 	private Datastore datastore;
 
 	@Override
 	public void init() {
 		datastore = new Datastore();
+		migrantArray = new JsonArray();
+		Gson gson = new Gson();
+		List<Migrant> migList = datastore.getAllMigrants(); 
+		
+		for(Migrant temp: migList) {
+			migrantArray.add(gson.toJsonTree(temp));
+		}
 	}
 
 	/**
@@ -29,9 +38,11 @@ public class ChartServlet extends HttpServlet{
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		response.setContentType("application/json");
+		response.getOutputStream().println(migrantArray.toString());
+		/*response.setContentType("application/json");
 		List<Migrant> migList = datastore.getAllMigrants(); 
 		Gson gson = new Gson();
 		String json = gson.toJson(migList);
-		response.getWriter().println(json);
+		response.getWriter().println(json); */
 	}
 }
