948dfbdf4e79b0d6a5f8d33c1f76830385f32aac
ctiscareno <ctisca1998@gmail.com>
2019-04-17 23:04:07 -0600

    'cheating'

--- src/main/java/com/google/codeu/servlets/ChartServlet.java
+++ src/main/java/com/google/codeu/servlets/ChartServlet.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.google.codeu.data.Datastore;
 import java.util.List;
+import java.util.Scanner;
+
 import com.google.codeu.data.Message;
 import com.google.codeu.data.Migrant;
 import com.google.gson.Gson;
@@ -25,11 +27,33 @@ public class ChartServlet extends HttpServlet{
 		datastore = new Datastore();
 		migrantArray = new JsonArray();
 		Gson gson = new Gson();
-		List<Migrant> migList = datastore.getAllMigrants(); 
+		Scanner scanner = new Scanner(getServletContext().getResourceAsStream("/WEB-INF/dataset.csv"));
+		while(scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			String[] cells = line.split(",");
+
+			// for (String cell : cells) {
+			//  System.out.println(cell);
+			// }
+
+			int id = Integer.parseInt(cells[0]);
+			String causeOfDeath = cells[1].equals("") ? " " : cells[1];
+			String origin = cells[2].equals("") ? " " : cells[2];
+			int numDead = cells[3].equals("") ? 0 : Integer.parseInt(cells[3]);
+			String incidentRegion = cells[4].equals("") ? " " : cells[4];
+			String date = cells[5].equals("") ? " " : cells[5];
+			double lat = cells[6].equals("") ? 0 : Double.parseDouble(cells[6]);
+			double lng = cells[7].equals("") ? 0 : Double.parseDouble(cells[7]);
+			Migrant mig = new Migrant(id, causeOfDeath, origin, numDead, incidentRegion, date, lat, lng);
+			
+			migrantArray.add(gson.toJsonTree(mig));
+		}
+
+		/*List<Migrant> migList = datastore.getAllMigrants(); 
 		
 		for(Migrant temp: migList) {
 			migrantArray.add(gson.toJsonTree(temp));
-		}
+		} */
 	}
 
 	/**
